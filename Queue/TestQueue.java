d5c29b833f52e2ff8bc89fd9ba37df0210fd9450
wangdong <wangdong@smartisan.com>
2017-01-25 18:10:21 +0800

    Add Queue test

---
+++ Queue/TestQueue.java
@@ -0,0 +1,23 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class TestQueue {
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<String>();
+        queue.offer("Hello");
+        queue.offer("World!");
+        queue.offer("你好！");
+        System.out.println(queue.size());
+        String str;
+        while((str=queue.poll())!=null){
+            System.out.print(str);
+        }
+        System.out.println();
+        System.out.println(queue.size());
+    }
+}
+/*
+ 3
+ HelloWorld!你好！
+ 0
+ */
